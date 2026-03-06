a6306407e7654b60bd874ecd86df2da39f5c18ed
aymenaamir <aamiraymen7@gmail.com>
2026-03-06 16:07:12 +0530

    use case one is done

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,8 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("welcome page ");
+        System.out.println("version :1");
+        System.out.println("system initialized ");
+    }
+
 }
